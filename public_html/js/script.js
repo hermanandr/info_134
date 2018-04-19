00618b82c6c29fe43072e7dae3cbfb4f13b60cfa
Edvard Øverland <edv.overland@gmail.com>
2018-04-19 10:57:42 +0200

    Laster map
    Trenger å fikse scriptet slik at den kaller forskjellige kart på de forskjellige sidene

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,36 +1,13 @@
 // Lenke til JSON-data.
-var url= "http://hotell.difi.no/api/json/bergen/dokart";
-
-// 'initMap()' itererer over 'toilets[]' og legger dem til på kartet. Da koordinatene er lagret som 'String'-verdier, konverteres de til tall ved hjelp av Number() (Edvard)
-function initMap(list){
-  console.log('init');
-  var bergen = {lat:60.394106, lng:5.324017}
-
-      var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 14,
-    center : bergen,
-    map: map
-  });
-
-  for(var i = 0; i < list.length; i++){
-    var marker = new google.maps.Marker({
-      position: {
-        lat:Number(list[i].latitude),
-        lng:Number(list[i].longitude)
-      },
-      label: (i + 1).toString(),
-      map: map
-    })
-    addInfo(marker, i);
-  }
-  addList();
-}
+var toaletter = "https://hotell.difi.no/api/json/bergen/dokart";
+var lekeplasser = "https://hotell.difi.no/api/json/bergen/lekeplasser?";
+var data = [];
 
 // legger til info om markøren til infovinduet. (Edvard)
-var addInfo = function(marker, i){
+function addInfo(list, marker, i){
   // stringen 'text' inneholder HTML-kode som vil vises i infovinduet (Edvard)
-  var text = "<div id='info'><h3>" + toilets[i].plassering + "</h3>"
-  + "<h4>" + toilets[i].adresse + "</h4> ";
+  var text = "<div id='info'><h3>" + list[i].plassering + "</h3>"
+  + "<h4>" + list[i].adresse + "</h4> ";
 
   marker.addListener('click', function() {
     if(marker.open != true){
@@ -50,9 +27,9 @@ var addInfo = function(marker, i){
 }
 
 // legger til en liste over alle de forskjellige markørene, navngitt etter plassering. (Edvard)
-function addList(){
-  for(var x = 0; x < toilets.length; x++){
-    var adr = toilets[x].plassering;
+function addList(list){
+  for(var x = 0; x < list.length; x++){
+    var adr = list[x].plassering;
     var text = document.createTextNode(adr);
     var toilet = document.createElement("li");
     toilet.appendChild(text);
@@ -63,14 +40,14 @@ function addList(){
 function search(searchObject) {
 	var searchResults  = [];
 	var searchParams = Object.keys(searchObject);
-	for(i=0; i < toilets.length; i++) {
+	for(i=0; i < list.length; i++) {
 		var truthChecker = [] // will contain boolean values "true" for each param checked.
 		for(y=0; y < searchParams.length; y++) {
-			if(toilets[i][searchParams[y]] == searchObject[searchParams[y]]) {
+			if(list[i][searchParams[y]] == searchObject[searchParams[y]]) {
 				truthChecker.push(true);
 			}
 			if(truthChecker.length == searchParams.length) { //if all params are true, person is pushed.
-				searchResults.push(toilets[i]);
+				searchResults.push(list[i]);
 			}
 		}
 	}
@@ -78,30 +55,59 @@ function search(searchObject) {
   console.log(searchResults);
 }
 
+// 'initMap()' itererer over 'list[]' og legger dem til på kartet. Da koordinatene er lagret som 'String'-verdier, konverteres de til tall ved hjelp av Number() (Edvard)
+function initMap(list){
+  var bergen = {lat:60.394106, lng:5.324017}
 
-function resetMap() {
-  initMap(toilets);
+      var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center : bergen,
+    map: map
+  });
+
+  for(var i = 0; i < list.length; i++){
+    var marker = new google.maps.Marker({
+      position: {
+        lat:Number(list[i].latitude),
+        lng:Number(list[i].longitude)
+      },
+      label: (i + 1).toString(),
+      map: map
+    })
+    addInfo(list, marker, i);
+  }
+  addList(list);
 }
 
 // Oppretter og sender en XML-request etter en URL, og returnerer dataen mottatt. (Edvard)
-function request(){
+function request(url){
   var xhr = new XMLHttpRequest();
+  var entries =[];
   xhr.open("GET", url);
   xhr.onreadystatechange = function(){
     if(xhr.readyState === 4 && xhr.status === 200){
       console.log("Type", xhr.getResponseHeader("Content-Type"));
       var ent = JSON.parse(xhr.responseText);
-      toilets = ent.entries;
-      return toilets;
+      entries = ent.entries;
+      console.log(entries);
+      updateArray(entries);
+      return entries;
     }
     else{
+      console.log("couldn't load dataset: " + url);
       return null;
     }
   }
     xhr.send();
-    // I tilfelle initMap() blir kjørt før 'toilets' er blitt gitt verdiene fra forespørselen, kalles den på ny etter den er blitt sendt. (Edvard)
-    initMap(toilets);
 }
 
-// Oppretter den globale variabelen 'toilets' som et Array med data fra XML-requestet. (Edvard)
-var toilets = request();
+function updateArray(array){
+  data = array;
+}
+
+function loadMap(url) {
+  request(url);
+  initMap(data);
+}
+
+loadMap(toaletter);
